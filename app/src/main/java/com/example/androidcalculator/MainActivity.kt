09695f6223f80b49860f6e4e400c210aaecc9214
SadScore <sergei.aulov7@gmail.com>
2025-01-07 01:33:27 +0300

    Update README.md

--- app/src/main/java/com/example/androidcalculator/MainActivity.kt
+++ app/src/main/java/com/example/androidcalculator/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.androidcalculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.app.*
+
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var toolbarMain: Toolbar
+
     private lateinit var hourFirstOperandET: EditText
     private lateinit var minuteFirstOperandET: EditText
     private lateinit var secondFirstOperandET: EditText
@@ -21,9 +31,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var buttonSumBTN: Button
     private lateinit var buttonDifBTN: Button
-    private lateinit var buttonResetBTN: Button
-    private lateinit var buttonExitBTN: Button
-
 
     private lateinit var resultTV: TextView
 
@@ -37,6 +44,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        toolbarMain = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbarMain)
+        title = "Калькулятор времени"
+        toolbarMain.subtitle = "Версия 2"
+        toolbarMain.setLogo(R.drawable.ic_calculate)
+
+
+
         hourFirstOperandET = findViewById(R.id.hourFirstOperandET)
         minuteFirstOperandET = findViewById(R.id.minuteFirstOperandET)
         secondFirstOperandET = findViewById(R.id.secondFirstOperandET)
@@ -46,15 +61,46 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         buttonSumBTN = findViewById(R.id.buttonSumBTN)
         buttonDifBTN = findViewById(R.id.buttonDifBTN)
-        buttonResetBTN = findViewById(R.id.buttonResetBTN)
-        buttonExitBTN = findViewById(R.id.buttonExitBTN)
 
         resultTV = findViewById(R.id.resultTV)
 
         buttonSumBTN.setOnClickListener(this)
         buttonDifBTN.setOnClickListener(this)
-        buttonResetBTN.setOnClickListener(this)
-        buttonExitBTN.setOnClickListener(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.resetManuMain -> {
+                hourFirstOperandET.text.clear()
+                minuteFirstOperandET.text.clear()
+                secondFirstOperandET.text.clear()
+                hourSecondOperandET.text.clear()
+                minuteSecondOperandET.text.clear()
+                secondSecondOperandET.text.clear()
+                resultTV.text = "Результат"
+                Toast.makeText(
+                    applicationContext,
+                    "Данные очищены",
+                    Toast.LENGTH_LONG
+                ).show()
+                resultTV.setTextColor(resources.getColor(R.color.black))
+            }
+            R.id.exitManuMain -> {
+                Toast.makeText(
+                    applicationContext,
+                    "Приложение закрыто",
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onClick(v: View?) {
@@ -78,24 +124,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         var secondSecond = secondSecondOperandET.text.toString().toInt()
 
 
+        resultTV.text = when (v?.id) {
 
-        var result = when (v?.id) {
-            R.id.buttonDifBTN -> TimeOperation(hourFirst, minuteFirst, secondFirst,
-                hourSecond,minuteSecond, secondSecond).dif()
-            R.id.buttonSumBTN -> TimeOperation(hourFirst, minuteFirst, secondFirst,
-                hourSecond,minuteSecond, secondSecond).sum()
-            R.id.buttonResetBTN -> {
-                hourFirstOperandET.text.clear()
-                minuteFirstOperandET.text.clear()
-                secondFirstOperandET.text.clear()
-                hourSecondOperandET.text.clear()
-                minuteSecondOperandET.text.clear()
-                secondSecondOperandET.text.clear()
-                check = false
-            }
-                R.id.buttonExitBTN -> finish()
-                else -> 0
+            R.id.buttonDifBTN -> TimeOperation(
+                hourFirst, minuteFirst, secondFirst,
+                hourSecond, minuteSecond, secondSecond
+            ).dif()
+
+            R.id.buttonSumBTN -> TimeOperation(
+                hourFirst, minuteFirst, secondFirst,
+                hourSecond, minuteSecond, secondSecond
+            ).sum()
+
+
+
+            else -> 0.toString()
+
         }
-        if (!check) resultTV.text = "Результат" else resultTV.text = result.toString()
+
+        if (resultTV.text != "Ошибка вычисления") {
+            resultTV.setTextColor(resources.getColor(R.color.redOne))
+        }
+        Toast.makeText(
+            applicationContext,
+            resultTV.text.toString(),
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
